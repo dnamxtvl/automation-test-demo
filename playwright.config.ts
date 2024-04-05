240b445b07eb41a49f1c07579e8d9d4455be7578
nam.dovan1 <nam.dovan1@vti.com.vn>
2024-04-05 16:26:46 +0700

    Change directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: [['list'], ['html'], ['json', {  outputFile: 'test-results.json' }],
+  reporter: [['html'], ['json', {  outputFile: './test-results/index.json' }],
     ['github']
   ],
   use: {
